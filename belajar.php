8f3d95d70ce4c35bb039cf76cf83b032b891e9fe
aufagunawan <aufahelmi08@gmail.com>
2021-02-24 21:41:55 +0700

    belajar-php

---
+++ belajar.php
@@ -0,0 +1,14 @@
+<?php
+
+ $nama = 'Rosalie Naurah';
+ $umur = 13;
+ $berat = 22.4;
+
+ echo 'Nama : ' . $nama;
+ echo '</br>';
+ echo 'Umur : ' . $umur;
+ echo '</br>';
+ echo 'Berat : ' . $berat;
+ echo '</br>';
+ echo "dokumen saya : ".$_SERVER["DOCUMENT_ROOT"];
+?>
